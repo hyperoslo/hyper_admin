50a29fe4168865a0e5f0a366c0d26ff6645cdbd3
Sindre Moen <sindrenm@gmail.com>
2014-08-27 00:09:02 +0200

    Create Person model in dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825171947) do
+ActiveRecord::Schema.define(version: 20140826220715) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "people", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ spec/dummy/spec/factories/people.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :person do
+    first_name "Ronnie"
+    last_name "Coleman"
+  end
+
+end
