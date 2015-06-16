29adeddd3f387cf6acfb87a72e32d9d6ee7c34b2
Stephen Benner <sbenner9@gmail.com>
2015-06-16 04:07:29 -0700

    Extracted `HyperAdmin::ValidationHelper` module into its own file, which was overdue (my bad). Initially it contained a mere few lines of code, but in retrospect that’s not a valid excuse for breaking best practice...

--- app/helpers/hyper_admin/form_helper.rb
+++ app/helpers/hyper_admin/form_helper.rb
@@ -1,62 +1,6 @@
-# todo: rspec
+# todo: create tests for everything in this file
 
 module HyperAdmin
-  module ValidationHelper
-
-    # This Hash defines the mappings from Rails validations to their corresponding
-    # AngularJS directives and HTML attributes. It follows a unique but simple contract.
-    #
-    # KEYS [Symbol] Identifiers of validations one would pass to the `validates` method
-    #   in an Active Record model file.
-    # VALUES [Array(String, Hash)] Array containing the identifier of equivalent AngularJS
-    #   validation directive, followed by an HTML attributes hash for its implementation.
-    #   Multiple directive identifiers may be specified by separating them with commas.
-    # VALUES [Proc] A lambda or Proc may be given instead, but it must evaluate to an
-    #   Array of the same format as previously described when called.
-    #
-    # Using a Proc allows for arbitrary transformation of the Rails validation data
-    #
-    TRANSLATIONS = {
-      presence: ['required', {required: true}],
-      length: ->(v) {
-        # todo: implement support for :wrong_length, :too_long, and :too_short
-        # todo: implement support for %{count} placeholder
-        # todo: implement support for tokenizer
-        # todo: implement support for :only_integer
-        # todo: oh god when do these options end
-        case
-          when v.options.key?(:minimum)
-            ['minlength', {'ng-minlength' => v.options[:minimum]}]
-          when v.options.key?(:maximum)
-            ['maxlength', {'ng-maxlength' => v.options[:maximum]}]
-          when v.options.key?(:in) || v.options.key?(:within)
-            range = v.options[:in] || v.options[:within]
-            ['minlength,maxlength', {'ng-minlength' => range.min, 'ng-maxlength' => range.max}]
-          when v.options.key?(:is)
-            ['minlength,maxlength', {'ng-minlength' => v.options[:is], 'ng-maxlength' => v.options[:is]}]
-        end
-      },
-      # todo: consider implementing support for :without option
-      format: ->(v) { ['pattern', {pattern: v.options[:with].source}] }
-    }
-
-    # This helper facilitates access to data in the TRANSLATIONS Hash, returning one or both of:
-    #
-    # @return [Array<String>] Collection of one or more AngularJS validation directive names
-    # @return [Hash{Symbol, String => String}] Hash of HTML attributes representing AngularJS
-    #   implementation of the validation (directives)
-    def translate_rails_validation(validation, output = {})
-      angular_output = TRANSLATIONS[validation.kind]
-      resolved_data = angular_output.respond_to?(:call) ? angular_output.call(validation) : angular_output
-      case output
-        when {into: :angular_validations} then resolved_data.first.split(',')
-        when {into: :angular_directives}  then resolved_data.last
-        when {}                           then resolved_data
-      end
-    end
-
-  end
-
   module FormHelper
     include ValidationHelper
 
--- doc/Validations.md
+++ doc/Validations.md
@@ -1,5 +1,5 @@
 # Client-side Rails validations
-This provides a summary and explanation of changes made in `feature/client-side-validations`.
+This file provides a summary and explanation of changes made in the [`feature/client-side-validations`][1] branch.
 
 ### Concept
 Though Rails and AngularJS offer validation features whose capabilities are quite similar, they represent two disparate paradigms. Rails validations are designed entirely for use on the server, while AngularJS is of course a client-side framework (although it also possess server-side validation tools). This is an attempt to bring Rails validations over to the client-side. With this update, validations specified in Rails models are automagically brought to life via AngularJS HTML validations. 
@@ -27,3 +27,6 @@ Only the most simple validations are implemented for this demo. They are listed 
 - Forms are produced dynamically on the client-side, using pre-rendered AngularJS templates. I realized that as a result, no object data is available to the templates unless specifically loaded into AngularJS beforehand. This is sufficient when the page requires little information about objects, but **for several reasons, I chose to redo the form templating code entirely, using an ERB-centric approach**.
 - AngularJS validations are implemented with HTML attributes and/or content added to form inputs. Composition of such elements is best handled with a view helper, and Rails already provides helpers that generate tags from an attributes `Hash`, which we can build off of.
 - In the partial `_form.html.erb`, I replaced the static AngularJS templates with ERB that generates a template for each field, instead of one template per field type. This is necessary because the inputs require data from the current `Resource` at render. It is also more DRY. This change does not affect AngularJS code or operation except that each template `id` is set to the attribute name, so in the `resourceForm` directive the `src` attribute of `ng-include` tag must be set to `'attr.key'`.
+
+
+[1]: https://github.com/SteveBenner/hyper_admin/tree/feature/client-side-validations
